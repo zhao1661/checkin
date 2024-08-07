a7996712ac76faf8b78de0ab1445855669c10da5
Zhao Zhe <115162585+zhao1661@users.noreply.github.com>
2024-08-07 17:05:11 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,50 @@
+const fetchWithTimeout = async (url, options, timeout = 5000) => {
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), timeout);
+  const response = await fetch(url, {
+    ...options,
+    signal: controller.signal,
+  });
+  clearTimeout(id);
+  return response;
+};
+
 const glados = async () => {
-  const cookie = process.env.GLADOS
-  if (!cookie) return
+  const cookie = process.env.GLADOS;
+  if (!cookie) return;
   try {
     const headers = {
       'cookie': cookie,
       'referer': 'https://glados.rocks/console/checkin',
       'user-agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
-    }
-    const checkin = await fetch('https://glados.rocks/api/user/checkin', {
+    };
+    const checkin = await fetchWithTimeout('https://glados.rocks/api/user/checkin', {
       method: 'POST',
       headers: { ...headers, 'content-type': 'application/json' },
       body: '{"token":"glados.one"}',
-    }).then((r) => r.json())
-    const status = await fetch('https://glados.rocks/api/user/status', {
+    }).then((r) => r.json());
+    const status = await fetchWithTimeout('https://glados.rocks/api/user/status', {
       method: 'GET',
       headers,
-    }).then((r) => r.json())
+    }).then((r) => r.json());
     return [
       'Checkin OK',
       `${checkin.message}`,
       `Left Days ${Number(status.data.leftDays)}`,
-    ]
+    ];
   } catch (error) {
     return [
       'Checkin Error',
       `${error}`,
       `<${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}>`,
-    ]
+    ];
   }
-}
+};
 
 const notify = async (contents) => {
-  const token = process.env.NOTIFY
-  if (!token || !contents) return
-  await fetch(`https://www.pushplus.plus/send`, {
+  const token = process.env.NOTIFY;
+  if (!token || !contents) return;
+  await fetchWithTimeout(`https://www.pushplus.plus/send`, {
     method: 'POST',
     headers: { 'content-type': 'application/json' },
     body: JSON.stringify({
@@ -42,11 +53,11 @@ const notify = async (contents) => {
       content: contents.join('<br>'),
       template: 'markdown',
     }),
-  })
-}
+  });
+};
 
 const main = async () => {
-  await notify(await glados())
-}
+  await notify(await glados());
+};
 
-main()
+main();
